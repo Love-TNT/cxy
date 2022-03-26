3219ce0fe6b6589bec65152710e46c6ab7c1d029
2,4,6三硝基甲苯 <88142364+TNT-Love@users.noreply.github.com>
2022-03-26 22:00:54 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||崔|心|月|我|喜|欢|你|:red_heart:|#rectangle|');
+      S.UI.simulate('|#countdown 3||崔|心|月|我|喜|欢|你|💜|#rectangle|');
     }
 
     S.Drawing.loop(function () {
